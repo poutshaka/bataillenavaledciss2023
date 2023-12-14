cf5070bc7b6152fe551ac3f17b7ad9f9e7e127b2
margotchab <153506591+margotchab@users.noreply.github.com>
2023-12-14 11:19:52 +0100

    ajout de JUnit pour faire des tests

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/TestJoueurAutoIntel.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/TestJoueurAutoIntel.java
@@ -8,14 +8,13 @@ public class TestJoueurAutoIntel {
     private GrilleNavale grille;
     private JoueurAutoIntelligent joueur;
 
-    @Before
     public void setUp() {
         // Initialisez vos objets nécessaires ici, par exemple une nouvelle grille
-        grille = new GrilleNavale(10);
+        grille = new GrilleNavale(10, new int[]{5, 4, 3, 3, 2});
         joueur = new JoueurAutoIntelligent(grille, "TestPlayer");
     }
 
-    @Test
+    
     public void testChoixAttaque() {
         // Testez le comportement de choixAttaque() en fonction des différents scénarios possibles
 
@@ -26,7 +25,7 @@ public class TestJoueurAutoIntel {
         assertTrue(grille.getTirs().contains(attaqueApresTouche));
 
         // Cas où le dernier tir n'a pas touché
-        joueur.retourAttaque(new Coordonnee(2, 2), Joueur.DANS_EAU);
+        joueur.retourAttaque(new Coordonnee(2, 2), Joueur.A_L_EAU);
         Coordonnee attaqueApresEau = joueur.choixAttaque();
         assertNotNull(attaqueApresEau);
         assertTrue(grille.getTirs().contains(attaqueApresEau));
