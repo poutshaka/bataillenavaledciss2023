c2d3bbfb8b4d04d21a897770d3cc2c67f6a9c4d7
margotchab <153506591+margotchab@users.noreply.github.com>
2023-12-14 11:30:07 +0100

    ajout d'une methode main

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/TestJoueurAutoIntel.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/TestJoueurAutoIntel.java
@@ -1,38 +1,38 @@
 import fr.uga.miashs.inff3.bataillenavale.*;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class TestJoueurAutoIntel {
 
     private GrilleNavale grille;
     private JoueurAutoIntelligent joueur;
 
-    @Before
     public void setUp() {
         // Initialisez vos objets nécessaires ici, par exemple une nouvelle grille
         grille = new GrilleNavale(10, new int[]{5, 4, 3, 3, 2});
         joueur = new JoueurAutoIntelligent(grille, "TestPlayer");
     }
 
-    @Test
     public void testChoixAttaque() {
         // Testez le comportement de choixAttaque() en fonction des différents scénarios possibles
 
         // Cas où le dernier tir a touché
         joueur.retourAttaque(new Coordonnee(1, 1), Joueur.TOUCHE);
         Coordonnee attaqueApresTouche = joueur.choixAttaque();
-        assertNotNull(attaqueApresTouche);
+        assertNonNull(attaqueApresTouche);
         assertTrue(grille.getTirs().contains(attaqueApresTouche));
 
         // Cas où le dernier tir n'a pas touché
         joueur.retourAttaque(new Coordonnee(2, 2), Joueur.A_L_EAU);
         Coordonnee attaqueApresEau = joueur.choixAttaque();
-        assertNotNull(attaqueApresEau);
+        assertNonNull(attaqueApresEau);
         assertTrue(grille.getTirs().contains(attaqueApresEau));
     }
 
-    // Ajoutez d'autres méthodes de test au besoin
+    public static void main(String[] args) {
+        TestJoueurAutoIntel test = new TestJoueurAutoIntel();
+        test.setUp();
+        test.testChoixAttaque();
 
+        // Ajoutez des appels à d'autres méthodes de test au besoin
+    }
 }
 
