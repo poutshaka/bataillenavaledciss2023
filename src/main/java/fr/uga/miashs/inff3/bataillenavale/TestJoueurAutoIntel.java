5afbc46647fb5a478af6a5d8738df44f07882926
margotchab <153506591+margotchab@users.noreply.github.com>
2023-12-15 11:36:59 +0100

    debug joueur auto youpi!

--- src/main/java/fr/uga/miashs/inff3/bataillenavale/TestJoueurAutoIntel.java
+++ src/main/java/fr/uga/miashs/inff3/bataillenavale/TestJoueurAutoIntel.java
@@ -5,8 +5,15 @@ public class TestJoueurAutoIntel {
 
     public static void main(String[] args) {
         // Créez une grille pour le test
-        GrilleNavale grille = new GrilleNavale(10, new int[]{5, 4, 3, 3, 2});
-
+       GrilleNavale grille = new GrilleNavale(10, new int[]{5, 4, 3, 3, 2});
+    	/* GrilleNavale grille = new GrilleNavale(10, 1);
+    	 grille.ajouteNavire(new Navire(new Coordonnee("A1"),1,false));
+    	 
+    	 System.out.println(grille);
+    	 
+    	 boolean res = grille.recoitTir(new Coordonnee("A1"));
+    	 System.out.println(res);*/
+    	 
         // Créez un joueur auto intelligent pour tester
         JoueurAutoIntelligent joueur = new JoueurAutoIntelligent(grille, "TestPlayer");
 
